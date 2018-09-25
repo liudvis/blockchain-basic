d030be39db4356d24881df32de022e8c610278d9
Liudvikas Rad <liudvikasr6@gmail.com>
2018-09-25 09:42:57 +0300

    m

--- DUMMY.py
+++
@@ -1,9 +0,0 @@
-class Food:
-    def __init__(self, name, kind):
-        self.name=name
-        self.kind=kind
-    def describe(self):
-        print('{}, {}'.format(self.name, self.kind))
-
-food = Food("banana", "fruit")
-food.describe()
